adab6ae54a5cf38b44fe7de862fd0835fbe2d87f
amemawueric <amemawueric199999@gmail.com>
2025-10-29 13:31:32 +0000

    exercise 4 done

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,63 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        int userInput = -1;
+
+        while (userInput != 0)
+        {
+            Console.WriteLine("Enter your number(0 to end): ");
+            string userResponse = Console.ReadLine();
+            userInput = int.Parse(userResponse);
+
+            if (userInput != 0)
+            {
+                numbers.Add(userInput);
+            }
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The max is: {max}");
+
+        int? smallestPositive = null;
+        foreach (int number in numbers)
+        {
+            if (number > 0 && (smallestPositive == null || number < smallestPositive))
+            {
+                smallestPositive = number;
+            }
+        }
+
+        if (smallestPositive.HasValue)
+        {
+            Console.WriteLine($"The smallest positive number is: {smallestPositive}");
+        }
+        else
+        {
+            Console.WriteLine("There are no positive numbers.");
+        }
     }
 }
+
