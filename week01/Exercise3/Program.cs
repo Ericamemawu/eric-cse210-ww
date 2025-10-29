b8e00d2f9bf6f4c71f98b73f1264ca008037e7e8
amemawueric <amemawueric199999@gmail.com>
2025-10-29 11:51:20 +0000

    completion of assignment 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,3 +1,4 @@
+
 using System;
 
 class Program
@@ -5,5 +6,71 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        bool playAgain = true;
+
+        while (playAgain)
+        {
+            {
+                Console.Write("What is the magic number? ");
+                int Number = int.Parse(Console.ReadLine());
+                int guess = -1;
+
+                while (guess != Number)
+                {
+                    Console.Write("What is your guess? ");
+                    guess = int.Parse(Console.ReadLine());
+
+                    if (guess < Number)
+                    {
+                        Console.WriteLine("Higher");
+                    }
+                    else if (guess > Number)
+                    {
+                        Console.WriteLine("Lower");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You guessed it!");
+                    }
+                }
+            }
+
+            {
+                Random randomGenerator = new Random();
+                int magicNumber = randomGenerator.Next(1, 101);
+                int userGuess = -1;
+
+                while (userGuess != magicNumber)
+                {
+                    Console.Write("What is your guess? ");
+                    userGuess = int.Parse(Console.ReadLine());
+
+                    if (magicNumber > userGuess)
+                    {
+                        Console.WriteLine("Higher");
+                    }
+                    else if (magicNumber < userGuess)
+                    {
+                        Console.WriteLine("Lower");
+                    }
+                    else
+                    {
+                        Console.WriteLine("You guessed it!");
+                    }
+                }
+            }
+
+            Console.Write("Would you like to play again? (yes/no): ");
+            string response = Console.ReadLine().ToLower();
+
+            while (response != "yes" && response != "no")
+            {
+                Console.Write("Invalid input. Please enter 'yes' or 'no': ");
+                response = Console.ReadLine().ToLower();
+            }
+
+            playAgain = response == "yes";
+        }
     }
 }
+
